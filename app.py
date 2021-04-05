ce0d08c9c3697c9323b29b1ac8276a1067ceef26
Cirineu Rodrigues <cirineu.rodriges@raccoon.ag>
2021-04-05 13:10:26 -0300

    Ajustes Heroku

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ from services.leads_services import LeadsServices
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/leads', methods=['GET'])
 def all_leads():
     return {
         'status': 'Ok!',
         'data': LeadsServices.read_csv()
     }
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to Rac Leads API</h1>"
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000
