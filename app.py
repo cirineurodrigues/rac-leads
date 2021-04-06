7c97d4b0071e087d867008f0b5d3e4cf06a5b5d4
Cirineu Rodrigues <cirineu.rodriges@raccoon.ag>
2021-04-06 13:59:05 -0300

    fix: cors

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
+from flask_cors = CORS
 
 from services.leads_services import LeadsServices
 from handlers.http_handlers import success_request, created_request, unprocessable_request
 
 app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 @app.route('/leads', methods=['GET'])
 def all_leads():
